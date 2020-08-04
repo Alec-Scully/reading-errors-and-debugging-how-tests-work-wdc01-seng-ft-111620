1a79be23939b84ef63c6992df7285449f7246f50
Alec Scully <scullyalec@gmail.com>
2020-08-04 23:45:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,9 @@
 # Add your variables here
 first_number = 4 
 puts first_number
+second_number = 2
+puts second_number
+sum = #{first_number} + #{second_number}
+difference = #{first_number} - #{second_number}
+product = #{first_number} * #{second_number}
+quotient = #{first_number} / #{second_number}
